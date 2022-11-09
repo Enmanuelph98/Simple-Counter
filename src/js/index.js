b69ccc067d9099d200d77275d8b567144951a296
Enmanuelph98 <enmanuelph98@gmail.com>
2022-11-09 20:23:24 +0000

    fix simple counter

--- src/js/index.js
+++ src/js/index.js
@@ -36,12 +36,12 @@ SimpleCounter.propTypes = {
 
 let counter = 0; 
 setInterval(function(){
-    const six = Math.floor(counter/10000);
-    const five = Math.floor(counter/10000);
-    const four = Math.floor(counter/1000);
-    const three = Math.floor(counter/100);
-    const two = Math.floor(counter/10);
-    const one = Math.floor(counter/1);
+    const six = Math.floor((counter/100000%10));
+    const five = Math.floor((counter/10000)%10);
+    const four = Math.floor((counter/1000)%10);
+    const three = Math.floor((counter/100)%10);
+    const two = Math.floor((counter/10)%10);
+    const one = Math.floor((counter/1)%10);
     console.log(six, five, four, three, two, one);
 
     counter++;
